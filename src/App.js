5f1ff58de50ac0e77ac7e0271842549106a20efb
Lenny Thach <lennythach1@gmail.com>
2022-06-09 17:41:08 -0700

    v1

--- src/App.js
+++ src/App.js
@@ -1,39 +1,43 @@
 // import logo from './logo.svg';
 import './App.css';
-import  { useState }  from 'react'
+import {useState} from 'react';
+
 function App() {
+  let [name, setName] = useState('Meiling');
 
-  let [name, setName] = useState('Lenny')
+  let [events, setEvents] = useState([
+    {event:'Hello, This is my first react webapp!', id:1},
+    {event:'This will be the start of my success!', id:2},
+    {event:'I cannot wait to see what will happens next!', id:3}
+  ])
 
-  const event = [
-    {title:'We bouts to party!', id:1},
-    {title:'You have poopy shoes!', id:2},
-    {title:'We workout for fun!', id:3},
-    {title:'This is how we do it!', id:4}
-    
-  ]
+  const changeName = () => setName('Lenny');
 
-  const changeName = () => {
-    return setName('Meiling')
+  const deleteEvent = (id) => {
+    setEvents((prevEvents)=>{
+      return prevEvents.filter((event)=>{
+        return id !== event.id
+      })
+    })
   }
 
+
   return (
-    
-    <div className="App">
-      <h1>Hello, My name is {name} </h1>
+  <div className='App'>
+    <h1>My name is {name}</h1>
+    <button onClick={changeName}>Change name</button>
 
-      <button onClick={changeName}>Change Name</button>
+    {events.map((event)=>{
+      return (
+        <div>
+          <h2>{event.event}</h2>
+          <button onClick={()=>deleteEvent(event.id)}>Delete Event</button>
+        </div>
+      )
+    })}
 
-      {
-        event.map((event)=>(
-          <div>
-            <h2>{event.id} {event.title}</h2>
-          </div>
-        ))
-      }
-
-    </div>
-  );
-}
+  </div>
+  )
+};
 
 export default App;
